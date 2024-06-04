58e95461a0d4f697e87c4b66ca4e95421d0abd31
samuxka <samukactto@gmail.com>
2024-06-03 22:02:08 -0300

    ain

--- game.py
+++ game.py
@@ -3,10 +3,13 @@ import random
 from src.utils.heros import choiceHero, hero
 from src.utils.combat import combat
 #import area - end
-def championSelectPlayer1():
-    print(f"O campeao escolhido pelo {player1} foi {selectHero[0]}")
-
-
-player1 = input("Como se chama campeao: ")
-selectHero = choiceHero()
-championSelectPlayer1()
+def championSelectPlayer():
+    player1 = input("Como se chama campeao: ")
+    selectHero = choiceHero()
+    print(f"O campeao escolhido pelo {player1} foi {selectHero[0]}\n")
+    print("==============================")
+    player2 = input("Como se chama campeao: ")
+    selectHero = choiceHero()
+    print(f"O campeao escolhido pelo {player2} foi {selectHero[0]}\n")
+    
+championSelectPlayer()
