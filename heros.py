776ca7d6e1754ff572fcdb9d925611da7b05a29d
Samuel <109113326+samuxka@users.noreply.github.com>
2024-06-05 13:53:32 -0300

    Update heros.py

--- heros.py
+++ heros.py
@@ -1,31 +1,32 @@
 def choiceHero():
-    print("Escolha seu herói:")
-    print("1. K'sante - Tanque/Lutador 🛡️🥊")
-    print("2. Sion - Tanque 🛡️")
-    print("3. Lee Sin - Lutador 🥊")
-    print("4. Veigar - Mago 🔮")
-    print("5. Ashe - Atirador 🏹")
-    print("6. Akali - Mago Assassino 🔮🗡️")
-
-    escolha = int(input("Digite o número da classe escolhida: "))
-
+    print(f"Escolha seu personagem:")
+    print(f"1. Saitama - Lutador 🥊")
+    print(f"2. Frank - Tanque 🛡️")
+    print(f"3. Luffy - Lutador 🥊")
+    print(f"4. Tristana - Atiradora 🏹")
+    print(f"5. Gojo - Mago 🔮")
+    print(f"6. Shaco - Assassino 🗡️")
+    
+    escolha = int(input("Digite o numero do personagem escolhido: "))
+    
     if escolha == 1:
-        return "K'sante 🛡️🥊", 610, 37, 32, 66, 0
+        #Nome, Vida, Armadura, Resistencia magica, Ataque fisico, ataque magico
+        return "Saitama 🥊", 635, 60, 54, 70, 0
     elif escolha == 2:
-        return "Sion 🛡️", 545, 32, 32, 68, 0
+        return "Frank 🛡️", 800, 76, 68, 53, 0
     elif escolha == 3:
-        return "Lee Sin 🥊", 575, 36, 32, 70, 0
+        return "Luffy 🥊", 530, 54, 43, 84, 0
     elif escolha == 4:
-        return "Veigar 🔮", 575, 23, 32, 52, 55
+        return "Tristana 🏹", 436, 35, 32, 64, 43
     elif escolha == 5:
-        return "Ashe 🏹", 570, 26, 30, 59, 0
+        return "Gojo 🔮", 420, 30, 28, 34, 70
     elif escolha == 6:
-        return "Akali 🔮🗡️", 570, 23, 37, 62, 50
+        return "Shaco 🗡️", 450, 35, 34, 67, 34
     else:
-        print("Escolha inválida!")
+        print("Opção invalida, digite um numero de 1 a 6!!")
         return choiceHero()
-
-class Hero:
+    
+class hero:
     def __init__(self, nome, life, shield, magicResistance, fisicDamage, magicDamage):
         self.nome = nome
         self.life = life
@@ -33,36 +34,36 @@ class Hero:
         self.magicResistance = magicResistance
         self.fisicDamage = fisicDamage
         self.magicDamage = magicDamage
-
+    
 attack = {
-    "K'sante 🛡️🥊": [
-        {"nome": "Fenda Sombria", "dano": 70, "chance": 0.8},
-        {"nome": "Corte da Lua", "dano": 90, "chance": 0.6},
-        {"nome": "Guardião das Sombras", "dano": 100, "chance": 0.3}
+    "Saitama 🥊":[
+        {"nome": "Soco Normal", "dano": 60},
+        {"nome": "Soco Quase Serio", "dano": 90},
+        {"nome": "Soco Serio", "dano": 150}
     ],
-    "Sion 🛡️": [
-        {"nome": "Investida do Decaído", "dano": 20, "chance": 1},
-        {"nome": "Grito do Assassino", "dano": 60, "chance": 0.8},
-        {"nome": "Investida Incontrolável", "dano": 150, "chance": 0.4}
+    "Frank 🛡️":[
+        {"nome": "Strong Right", "dano": 40},
+        {"nome": "Frank Fireball", "dano": 80},
+        {"nome": "Franky Radical Beam ", "dano": 120}
     ],
-    "Lee Sin 🥊": [
-        {"nome": "Onda Sônica", "dano": 50, "chance": 0.7},
-        {"nome": "Tempestade", "dano": 90, "chance": 0.6},
-        {"nome": "Guardião das Sombras", "dano": 100, "chance": 0.3}
+    "Luffy 🥊":[
+        {"nome": "Gatling", "dano": 54},
+        {"nome": "Red Hawk", "dano": 77},
+        {"nome": "King Kong Gun", "dano": 140}
     ],
-    "Veigar 🔮": [
-        {"nome": "Explosão Ígnea", "dano": 60, "chance": 0.6},
-        {"nome": "Morte Súbita", "dano": 70, "chance": 0.8},
-        {"nome": "Desprezo Primordial", "dano": 175, "chance": 0.3}
+    "Tristana 🏹":[
+        {"nome": "Tiro Explosivo", "dano": 37},
+        {"nome": "Tiro Rapido", "dano": 68},
+        {"nome": "Tiro Destruidor", "dano": 200}
     ],
-    "Ashe 🏹": [
-        {"nome": "Flecha de Gelo", "dano": 50, "chance": 1},
-        {"nome": "Frenesi de Guinsoo", "dano": 60, "chance": 0.8},
-        {"nome": "Flecha de Cristal Encantada", "dano": 200, "chance": 0.2}
+    "Gojo 🔮":[
+        {"nome": "Limitless", "dano": 40},
+        {"nome": "Six Eyes", "dano": 90},
+        {"nome": "Unlimited Void", "dano": 220}
     ],
-    "Akali 🔮🗡️": [
-        {"nome": "Marca do Assassino", "dano": 65, "chance": 0.8},
-        {"nome": "Investida Shuriken", "dano": 77, "chance": 0.6},
-        {"nome": "Investida dos Cinco Pontos", "dano": 125, "chance": 0.3}
+    "Shaco 🗡️":[
+        {"nome": "Jack In The Box", "dano": 36},
+        {"nome": "Two-Shiv Poison", "dano": 60},
+        {"nome": "Hallucinate", "dano": 110}
     ],
 }
