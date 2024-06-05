05e9d797dea52f81b7d39fbde8bd8ed12b7e4280
Samuel <109113326+samuxka@users.noreply.github.com>
2024-06-05 13:51:39 -0300

    Update game.py

--- game.py
+++ game.py
@@ -1,48 +1,52 @@
 import random
-from heros import choiceHero, Hero
+from heros import choiceHero, hero
 from combat import calculateDamage, combat
 
 def welcomeToGame():
-    print("BEM-VINDO AO TORNEIO DO PODER!!! 🤼‍♂️🥊🏆")
-    print("Está preparado para o que está te esperando????")
-    preparado = input("Está preparado? \ny/n: ")
+    print("=============================================================================\n")
+    print("BEM VINDO AO TORNEIO DO PODER!!! 🤼🏆")
+    print("Esta preparado para o que esta te esperando???")
+    preparado=input("Esta preparado???\nY/N: ")
     if preparado == "y":
-        print("Perfeito! Vamos começar a jogar! Chame um amigo para batalhar com você.\n")
+        print("PERFEITO!!! Vamos começar a jogar! Chame um amigo para batalhar com voce!!!\n")
+        print("=============================================================================\n")
     elif preparado == "n":
-        print("Não está preparado ainda???? Então prepare-se para a batalha!!!!\n")
+        print("NÃO ESTA PREPARADO??? Então prepare-se para a batalha\n")
         return welcomeToGame()
     else:
-        print("Opção inválida!!\n")
+        print("Opção invalida!!! Digite y ou n\n")
         return welcomeToGame()
-
-def championSelectPlayer(player_num):
+    
+def championSelectPlayer(playerNum):
     nome, life, shield, magicResistance, fisicDamage, magicDamage = choiceHero()
-    print(f"Ótima escolha, o campeão escolhido foi {nome}\n")
-    return Hero(nome, life, shield, magicResistance, fisicDamage, magicDamage)
+    print(f"Otima escolha, o campeão escolhido foi {nome}\n")
+    print("=============================================================================\n")
+    return hero(nome, life, shield, magicResistance, fisicDamage, magicDamage)
 
 def evenOrOdd(jogador1, jogador2):
-    print("Escolha, Par ou Ímpar?")
-    print("1. Ímpar")
-    print("2. Par")
-    escolha = int(input("Digite sua escolha: "))
+    print("Esolha, Par ou Impar? 🎲")
+    print("1. Impar\n2. Par")
+    escolha=int(input("Digite sua escolha: "))
     if escolha == 1:
-        escolhaJogador1 = "ímpar"
+        escolhaJogador1 =  "Impar"
     elif escolha == 2:
-        escolhaJogador1 = "par"
+        escolhaJogador1 = "Par"
     else:
-        print("Opção inválida!!!")
+        print("Opção invalida!!!")
         return evenOrOdd(jogador1, jogador2)
-
+    
     diceResult = random.randint(1, 6)
-
+    
     print(f"O jogador {jogador1.nome} escolheu {escolhaJogador1}")
     print(f"Resultado do dado: {diceResult}")
-
-    if (diceResult % 2 == 0 and escolhaJogador1 == "par") or (diceResult % 2 != 0 and escolhaJogador1 == "ímpar"):
+    
+    if (diceResult % 2 == 0 and escolhaJogador1 == "Par") or (diceResult % 2 != 0 and escolhaJogador1 == "Impar"):
         print(f"{jogador1.nome} começa atacando\n")
+        print("=============================================================================\n")
         return jogador1, jogador2
     else:
         print(f"{jogador2.nome} começa atacando\n")
+        print("=============================================================================\n")
         return jogador2, jogador1
 
 if __name__ == "__main__":
